0f3980827cff92066f28ad5c3a7c6bf4340db425
brayanACDC <brayanc5747@gmail.com>
2024-05-03 12:47:04 -0500

    array de 50 objetos del proyecto

---
+++ main.js
@@ -0,0 +1,462 @@
+[
+  {
+    nombre_libro: "El Último Dragón",
+    img: "./img/img_1.jpg",
+    categoria: "fantasia",
+    codigo: "LpHHH90",
+    precio: 75000,
+    descripcion:
+      "En un mundo dominado por la oscuridad, un joven héroe se alza para enfrentar al último dragón y restaurar la paz y la justicia en su reino.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Templo",
+    img: "./img/img_5.jpg",
+    categoria: "misterio",
+    codigo: "LpJJJ12",
+    precio: 55000,
+    descripcion:
+      "Una expedición arqueológica desentierra un antiguo secreto oculto bajo las ruinas de un templo perdido, desatando una serie de eventos inexplicables y peligros mortales.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del Bosque",
+    img: "./img/img_7.jpg",
+    categoria: "fantasia",
+    codigo: "LpKKK23",
+    precio: 49000,
+    descripcion:
+      "Una joven valiente se aventura en un bosque encantado en busca de su hermano perdido, descubriendo un mundo de magia y peligros ocultos en lo más profundo del bosque.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Destino del indio",
+    img: "./img/img_10.jpg",
+    categoria: "fantasia",
+    codigo: "LpTTT12",
+    precio: 72000,
+    descripcion:
+      "Un guerrero legendario se enfrenta a su destino final en una batalla épica por el alma de su reino, enfrentándose a enemigos mortales y desafíos imposibles en su lucha por la libertad y la justicia.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Reina del Desierto",
+    img: "./img/img_13.jpg",
+    categoria: "aventura",
+    codigo: "LpNNN56",
+    precio: 65000,
+    descripcion:
+      "Una joven guerrera se convierte en la última esperanza de su pueblo cuando un ejército invasor amenaza con destruir su hogar en el desierto, enfrentándose a peligros mortales y desafíos imposibles en su lucha por la libertad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Lago de Tota",
+    img: "./img/img_15.jpg",
+    categoria: "misterio",
+    codigo: "LpOOO67",
+    precio: 54000,
+    descripcion:
+      "Una serie de extrañas desapariciones en un tranquilo pueblo a orillas de un lago desatan una investigación policial llena de giros inesperados y oscuros secretos.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "El Último Amanecer",
+    img: "./img/img_2.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpPPP78",
+    precio: 69000,
+    descripcion:
+      "En un futuro distópico donde el sol se está apagando, un grupo de valientes astronautas se embarca en una misión desesperada para salvar a la humanidad de la extinción, enfrentándose a peligros cósmicos y desafíos imposibles en el espacio profundo.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Castillo",
+    img: "./img/img_4.jpg",
+    categoria: "misterio",
+    codigo: "LpQQQ89",
+    precio: 55000,
+    descripcion:
+      "Un oscuro secreto se esconde tras los muros de un antiguo castillo, atrayendo a un grupo de investigadores que se ven envueltos en una red de misterios y traiciones.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Hija del Rey",
+    img: "./img/img_6.jpg",
+    categoria: "fantasia",
+    codigo: "LpRRR90",
+    precio: 67000,
+    descripcion:
+      "Una joven princesa se embarca en una aventura épica para reclamar su derecho al trono, enfrentándose a enemigos mortales y desafíos imposibles en su camino hacia el poder.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del Valle de la Muerte",
+    img: "./img/img_8.jpg",
+    categoria: "misterio",
+    codigo: "LpSSS01",
+    precio: 49000,
+    descripcion:
+      "Un detective brillante investiga una serie de extrañas desapariciones en un tranquilo valle rural, descubriendo una red de secretos oscuros y peligros mortales que amenazan con destruir todo lo que conoce.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Susurro del Silencio",
+    img: "./img/img_11.jpg",
+    categoria: "suspenso",
+    codigo: "LpMMM45",
+    precio: 59000,
+    descripcion:
+      "Un oscuro secreto del pasado regresa para atormentar a una pequeña comunidad, desencadenando una serie de eventos que pondrán a prueba la voluntad y el coraje de sus habitantes.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "La Promesa del Guerrero",
+    img: "./img/img_16.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAB12",
+    precio: 62000,
+    descripcion:
+      "Un guerrero renegado hace una promesa de venganza contra un imperio corrupto, enfrentándose a enemigos mortales y desafíos imposibles en su búsqueda de justicia y redención.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Bosque",
+    img: "./img/img_17.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAC23",
+    precio: 59000,
+    descripcion:
+      "Una joven valiente se aventura en un bosque encantado en busca de su hermano perdido, descubriendo un mundo de magia y peligros ocultos en lo más profundo del bosque.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Destino del Guerrero Chifurro",
+    img: "./img/img_18.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAD34",
+    precio: 72000,
+    descripcion:
+      "Un guerrero legendario se enfrenta a su destino final en una batalla épica por el alma de su reino, enfrentándose a enemigos mortales y desafíos imposibles en su lucha por la libertad y la justicia.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Reina del Desierto sin Agua",
+    img: "./img/img_19.jpg",
+    categoria: "aventura",
+    codigo: "LpAAE45",
+    precio: 65000,
+    descripcion:
+      "Una joven guerrera se convierte en la última esperanza de su pueblo cuando un ejército invasor amenaza con destruir su hogar en el desierto, enfrentándose a peligros mortales y desafíos imposibles en su lucha por la libertad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Lago Calima",
+    img: "./img/img_20.jpg",
+    categoria: "misterio",
+    codigo: "LpAAF56",
+    precio: 54000,
+    descripcion:
+      "Una serie de extrañas desapariciones en un tranquilo pueblo a orillas de un lago desatan una investigación policial llena de giros inesperados y oscuros secretos.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "El Último Amanecer del Cielo",
+    img: "./img/img_21.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAG67",
+    precio: 69000,
+    descripcion:
+      "En un futuro distópico donde el sol se está apagando, un grupo de valientes astronautas se embarca en una misión desesperada para salvar a la humanidad de la extinción, enfrentándose a peligros cósmicos y desafíos imposibles en el espacio profundo.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Castillo de la Noche",
+    img: "./img/img_22.jpg",
+    categoria: "misterio",
+    codigo: "LpAAH78",
+    precio: 55000,
+    descripcion:
+      "Un oscuro secreto se esconde tras los muros de un antiguo castillo, atrayendo a un grupo de investigadores que se ven envueltos en una red de misterios y traiciones.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Hija del Rey",
+    img: "./img/img_23.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAI89",
+    precio: 67000,
+    descripcion:
+      "Una joven princesa se embarca en una aventura épica para reclamar su derecho al trono, enfrentándose a enemigos mortales y desafíos imposibles en su camino hacia el poder.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del Valle Verde",
+    img: "./img/img_24.jpg",
+    categoria: "misterio",
+    codigo: "LpAAJ90",
+    precio: 49000,
+    descripcion:
+      "Un detective brillante investiga una serie de extrañas desapariciones en un tranquilo valle rural, descubriendo una red de secretos oscuros y peligros mortales que amenazan con destruir todo lo que conoce.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Susurro de Silencio",
+    img: "./img/img_25.jpg",
+    categoria: "suspenso",
+    codigo: "LpAAK01",
+    precio: 59000,
+    descripcion:
+      "Un oscuro secreto del pasado regresa para atormentar a una pequeña comunidad, desencadenando una serie de eventos que pondrán a prueba la voluntad y el coraje de sus habitantes.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Guerrero de la Noche",
+    img: "./img/img_26.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAL12",
+    precio: 75000,
+    descripcion:
+      "En un mundo asolado por la guerra, un joven huérfano descubre su destino como el último guerrero de una antigua profecía, enfrentándose a monstruos y magia oscura en su búsqueda de la verdad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del lago",
+    img: "./img/img_27.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAM23",
+    precio: 49000,
+    descripcion:
+      "Una joven valiente se aventura en un bosque encantado en busca de su hermano perdido, descubriendo un mundo de magia y peligros ocultos en lo más profundo del bosque.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Destino del Guerrero",
+    img: "./img/img_28.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAN34",
+    precio: 72000,
+    descripcion:
+      "Un guerrero legendario se enfrenta a su destino final en una batalla épica por el alma de su reino, enfrentándose a enemigos mortales y desafíos imposibles en su lucha por la libertad y la justicia.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Reina del Desierto de la Arena",
+    img: "./img/img_29.jpg",
+    categoria: "aventura",
+    codigo: "LpAAO45",
+    precio: 65000,
+    descripcion:
+      "Una joven guerrera se convierte en la última esperanza de su pueblo cuando un ejército invasor amenaza con destruir su hogar en el desierto, enfrentándose a peligros mortales y desafíos imposibles en su lucha por la libertad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Lago Nes",
+    img: "./img/img_30.jpg",
+    categoria: "misterio",
+    codigo: "LpAAP56",
+    precio: 54000,
+    descripcion:
+      "Una serie de extrañas desapariciones en un tranquilo pueblo a orillas de un lago desatan una investigación policial llena de giros inesperados y oscuros secretos.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "El Último Amanecer del Sol",
+    img: "./img/img_31.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAQ67",
+    precio: 69000,
+    descripcion:
+      "En un futuro distópico donde el sol se está apagando, un grupo de valientes astronautas se embarca en una misión desesperada para salvar a la humanidad de la extinción, enfrentándose a peligros cósmicos y desafíos imposibles en el espacio profundo.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Castillo de Arena",
+    img: "./img/img_32.jpg",
+    categoria: "misterio",
+    codigo: "LpAAR78",
+    precio: 55000,
+    descripcion:
+      "Un oscuro secreto se esconde tras los muros de un antiguo castillo, atrayendo a un grupo de investigadores que se ven envueltos en una red de misterios y traiciones.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Hija del Rey",
+    img: "./img/img_33.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAS89",
+    precio: 67000,
+    descripcion:
+      "Una joven princesa se embarca en una aventura épica para reclamar su derecho al trono, enfrentándose a enemigos mortales y desafíos imposibles en su camino hacia el poder.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del Valle de los Negros",
+    img: "./img/img_34.jpg",
+    categoria: "misterio",
+    codigo: "LpAAT90",
+    precio: 49000,
+    descripcion:
+      "Un detective brillante investiga una serie de extrañas desapariciones en un tranquilo valle rural, descubriendo una red de secretos oscuros y peligros mortales que amenazan con destruir todo lo que conoce.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Susurro de Pepe",
+    img: "./img/img_35.jpg",
+    categoria: "suspenso",
+    codigo: "LpAAU01",
+    precio: 59000,
+    descripcion:
+      "Un oscuro secreto del pasado regresa para atormentar a una pequeña comunidad, desencadenando una serie de eventos que pondrán a prueba la voluntad y el coraje de sus habitantes.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Guerrero del Dia",
+    img: "./img/img_36.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAV12",
+    precio: 75000,
+    descripcion:
+      "En un mundo asolado por la guerra, un joven huérfano descubre su destino como el último guerrero de una antigua profecía, enfrentándose a monstruos y magia oscura en su búsqueda de la verdad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del monte",
+    img: "./img/img_37.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAW23",
+    precio: 49000,
+    descripcion:
+      "Una joven valiente se aventura en un bosque encantado en busca de su hermano perdido, descubriendo un mundo de magia y peligros ocultos en lo más profundo del bosque.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Destino del Guerrero Hecarim",
+    img: "./img/img_38.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAX34",
+    precio: 72000,
+    descripcion:
+      "Un guerrero legendario se enfrenta a su destino final en una batalla épica por el alma de su reino, enfrentándose a enemigos mortales y desafíos imposibles en su lucha por la libertad y la justicia.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Reina del Desierto de Madagascar",
+    img: "./img/img_39.jpg",
+    categoria: "aventura",
+    codigo: "LpAAY45",
+    precio: 65000,
+    descripcion:
+      "Una joven guerrera se convierte en la última esperanza de su pueblo cuando un ejército invasor amenaza con destruir su hogar en el desierto, enfrentándose a peligros mortales y desafíos imposibles en su lucha por la libertad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Lago sin Agua",
+    img: "./img/img_40.jpg",
+    categoria: "misterio",
+    codigo: "LpAAZ56",
+    precio: 54000,
+    descripcion:
+      "Una serie de extrañas desapariciones en un tranquilo pueblo a orillas de un lago desatan una investigación policial llena de giros inesperados y oscuros secretos.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "El Último Amanecer del sin Dientes",
+    img: "./img/img_41.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAA67",
+    precio: 69000,
+    descripcion:
+      "En un futuro distópico donde el sol se está apagando, un grupo de valientes astronautas se embarca en una misión desesperada para salvar a la humanidad de la extinción, enfrentándose a peligros cósmicos y desafíos imposibles en el espacio profundo.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Castillo de Drakula",
+    img: "./img/img_42.jpg",
+    categoria: "misterio",
+    codigo: "LpAAB78",
+    precio: 55000,
+    descripcion:
+      "Un oscuro secreto se esconde tras los muros de un antiguo castillo, atrayendo a un grupo de investigadores que se ven envueltos en una red de misterios y traiciones.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Hija del Rey",
+    img: "./img/img_43.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAC89",
+    precio: 67000,
+    descripcion:
+      "Una joven princesa se embarca en una aventura épica para reclamar su derecho al trono, enfrentándose a enemigos mortales y desafíos imposibles en su camino hacia el poder.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del Valle",
+    img: "./img/img_44.jpg",
+    categoria: "misterio",
+    codigo: "LpAAD90",
+    precio: 49000,
+    descripcion:
+      "Un detective brillante investiga una serie de extrañas desapariciones en un tranquilo valle rural, descubriendo una red de secretos oscuros y peligros mortales que amenazan con destruir todo lo que conoce.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Susurro",
+    img: "./img/img_45.jpg",
+    categoria: "suspenso",
+    codigo: "LpAAE01",
+    precio: 59000,
+    descripcion:
+      "Un oscuro secreto del pasado regresa para atormentar a una pequeña comunidad, desencadenando una serie de eventos que pondrán a prueba la voluntad y el coraje de sus habitantes.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Último Guerrero Dragon",
+    img: "./img/img_46.jpg",
+    categoria: "ciencia ficcion",
+    codigo: "LpAAF12",
+    precio: 75000,
+    descripcion:
+      "En un mundo asolado por la guerra, un joven huérfano descubre su destino como el último guerrero de una antigua profecía, enfrentándose a monstruos y magia oscura en su búsqueda de la verdad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Secreto del toto",
+    img: "./img/img_47.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAG23",
+    precio: 49000,
+    descripcion:
+      "Una joven valiente se aventura en un bosque encantado en busca de su hermano perdido, descubriendo un mundo de magia y peligros ocultos en lo más profundo del bosque.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Destino del Guerrero Dragon",
+    img: "./img/img_48.jpg",
+    categoria: "fantasia",
+    codigo: "LpAAH34",
+    precio: 72000,
+    descripcion:
+      "Un guerrero legendario se enfrenta a su destino final en una batalla épica por el alma de su reino, enfrentándose a enemigos mortales y desafíos imposibles en su lucha por la libertad y la justicia.",
+    disponibilidad: false,
+  },
+  {
+    nombre_libro: "La Reina del Desierto sin Tesorros ni Tierras",
+    img: "./img/img_49.jpg",
+    categoria: "aventura",
+    codigo: "LpAAI45",
+    precio: 65000,
+    descripcion:
+      "Una joven guerrera se convierte en la última esperanza de su pueblo cuando un ejército invasor amenaza con destruir su hogar en el desierto, enfrentándose a peligros mortales y desafíos imposibles en su lucha por la libertad.",
+    disponibilidad: true,
+  },
+  {
+    nombre_libro: "El Misterio del Lago de Pripra",
+    img: "./img/img_50.jpg",
+    categoria: "misterio",
+    codigo: "LpAAJ56",
+    precio: 54000,
+    descripcion:
+      "Una serie de extrañas desapariciones en un tranquilo pueblo a orillas de un lago desatan una investigación policial llena de giros inesperados y oscuros secretos.",
+    disponibilidad: false,
+  },
+];
